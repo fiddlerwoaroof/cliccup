3057437b30590425cd2b820a24d117075d4f86de
Edward Langley <el-github@elangley.org>
2023-08-30 23:43:14 -0700

    refactor: no more serapeum

--- cliccup.lisp
+++ cliccup.lisp
@@ -8,12 +8,35 @@
            #:make-text-node))
 (in-package :fwoar.cliccup)
 
+(defun invert-case (it)
+  (map 'string
+       (lambda (c)
+         (cond
+           ((upper-case-p c) (char-downcase c))
+           ((lower-case-p c) (char-upcase c))
+           (c)))
+       it))
+
+(defun split-dots (s)
+  (declare (optimize speed (debug 1)))
+  (check-type s string)
+  (etypecase s
+    (simple-string
+     #1=(loop for r-pos = (position #\. s)
+                then (position #\. s :start (1+ pos))
+              for r-old = nil then pos
+              for pos = (or r-pos (length s))
+              for old = (or r-old -1)
+              collect (subseq s (1+ old) pos)
+              while (< pos (length s))))
+    (string #1#)))
+
 (defun unpack-vector (vec)
   (let* ((length (length vec))
          (raw-tag (elt vec 0))
          (tag (etypecase raw-tag
                 (string raw-tag)
-                (symbol (serapeum:string-invert-case raw-tag))))
+                (symbol (invert-case raw-tag))))
          (attrs-p (and (> length 1)
                        (listp (elt vec 1))))
          (attrs (when attrs-p
@@ -41,10 +64,10 @@
   (destructuring-bind (head tail)
       (fwoar.string-utils:partition #\@ tag)
     (let* ((tail-parsed (when tail
-                          (fwoar.string-utils:split #\. tail)))
+                          (split-dots tail)))
            (id (when tail
                  (elt tail-parsed 0)))
-           (head-parsed (fwoar.string-utils:split #\. head))
+           (head-parsed (split-dots head))
            (tag (elt head-parsed 0))
            (classes (concatenate 'list
                                  (subseq head-parsed 1)
@@ -65,13 +88,13 @@
           (when classes
             (set-attribute tag
                            "class"
-                           (serapeum:string-join classes #\space)))
+                           (format nil "~{~a~^ ~}" classes)))
           (loop for (key value) on attrs by 'cddr
                 do
                    (set-attribute tag
                                   (typecase key
                                     (string key)
-                                    (symbol (serapeum:string-invert-case key)))
+                                    (symbol (invert-case key)))
                                   value))
           (map nil
                (lambda (child)
